9727162fd7ccf5698f6f01d2d2dd474c32323e10
Tyler Flowers <gamingguycontact@gmail.com>
2019-07-15 18:55:38 -0400

    Revert "Add files via upload"
    This reverts commit 34af0cac77b8c2b6f72758c625b0e3cf00940983.

--- src/main/java/net/ggtylerr/pingpong/Initializer.java
+++ src/main/java/net/ggtylerr/pingpong/Initializer.java
@@ -3,9 +3,12 @@
 package net.ggtylerr.pingpong;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.registry.CommandRegistry;
+import net.ggtylerr.pingpong.commands.Ping;
 
 public class Initializer implements ModInitializer {
 	@Override
 	public void onInitialize() {
+	    CommandRegistry.INSTANCE.register(false, Ping::register);
 	}
 }
